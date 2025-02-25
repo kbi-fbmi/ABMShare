a47e7d4901ceee5d448d1b03928691dd88cf9a9c
Jedimik_Laptop_Linux <krajca.tomascz@gmail.com>
2025-02-25 14:28:14 +0100

    simplified starting

--- abmShare_sandbox/start.sh
+++ abmShare_sandbox/start.sh
@@ -2,7 +2,7 @@
 
 unset VALIDATION
 
-while getopts "v:" flag
+while getopts "v:p:c:s:" flag
 do
     case "${flag}" in
         v) 
@@ -15,33 +15,48 @@ do
                 exit 1
             fi
             ;;
+        p)
+            parent_dir=${OPTARG}        
+            ;;
+        c)
+            mainconfig=${OPTARG}            
+            ;;
+        s)
+            script_path=${OPTARG}
+            ;;
         *) 
-            echo "Usage: scriptname [-v true|false]"
+            echo "Usage: scriptname [-v true|false] [-p parent_dir] [-c mainconfig] [-s execution_script_path]"
             exit 1
             ;;
     esac
 done
 
-# Get the directory of the current script and go two directories back
-parent_dir=$(dirname "$(dirname "$(readlink -f "$0")")")
+# Get the directory of the parrent_dir, defaultly
+if [ -z "$parent_dir" ]; then
+    parent_dir=$(dirname "$(dirname "$(readlink -f "$0")")")
+fi
 
 # Path to virtual environment
 source "$parent_dir/.venv/bin/activate" # Specify the full path, or let it based on the original structure
 # Chunk of code for getting timestamp
 dt=$(date '+%d_%m_%Y_%H_%M')
 # Filepath for python starting script
-filepath="$parent_dir/extensions/abm_share_start.py"
+if [ -z "$script_path" ]; then
+    script_path="$parent_dir/abmshare/abm_share_start.py"
+fi
 # Filepath for main configuration file
-mainconfig="$parent_dir/ShareSim/input_data/main_configuration.json"
+if [ -z "$mainconfig" ]; then
+    mainconfig="$parent_dir/abmShare_sandbox/input_data/main_configuration.json"
+fi
 # Needed process
-name="$parent_dir/ShareSim/nohup${dt}.out"
+name="$parent_dir/abmShare_sandbox/nohup${dt}.out"
 
 # Check the state of VALIDATION and act accordingly
 if [ "${VALIDATION}" == "true" ]; then
-    nohup python3 $filepath -c $mainconfig -v $validation &> $name &
+    nohup python3 $script_path -c $mainconfig -v $validation &> $name &
 elif [ "${VALIDATION}" == "false" ]; then # If v_flag is not set, call the Python script with False 
-    nohup python3 $filepath -c $mainconfig -v $validation &> $name &
+    nohup python3 $script_path -c $mainconfig -v $validation &> $name &
 else
-    nohup python3 $filepath -c $mainconfig &> $name &
+    nohup python3 $script_path -c $mainconfig &> $name &
 fi
 tail -f "nohup${dt}.out"
